fafed1b3db282f70e696e413b990c3233b902185
Zufan <zufanawushu@gmail.com>
2022-09-08 08:44:35 -0700

    put it

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Rturn: 0 after printing the function
+ */
+int main(void)
+{puts("\"programing is like building a multilingual puzzle");
+	return (0);
+}
