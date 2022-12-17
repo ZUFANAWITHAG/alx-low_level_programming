4b2d8b80198cbbc1d4f2a39e00646b5e867911e7
Zufan <zufanawushu@gmail.com>
2022-12-17 12:37:21 -0800

    9 Task

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,7 +12,7 @@ int main(void)
 
 	while (c < 10)
 	{
-		putchar(48 + c)
+		putchar(48 + c);
 		if (c != 9)
 		{
 			putchar(',');
@@ -20,6 +20,6 @@ int main(void)
 		}
 		c++;
 	}
-	putchar('\n')
+	putchar('\n');
 	return (0);
 }
