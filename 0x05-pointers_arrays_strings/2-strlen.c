e82541ec17d9b71ec367fd77138726ee32de5e47
Zufan <zufanawushu@gmail.com>
2022-12-19 05:37:23 -0800

    2 Task

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,16 +2,18 @@
 
 /**
  * _strlen - Function that returns the length of a string
- *
  * @s: this is the input string
- *
  * Return: Length of the string
  */
 int _strlen(char *s)
 {
-	int index;
-	
-	for (inde  = 0; s[index] != '\0' ; index++);
-               	
-	return (index);
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
 }
