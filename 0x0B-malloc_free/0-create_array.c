488139c219ad32d945c38ffeaecc71c011d0b317
Zufan <zufanawushu@gmail.com>
2023-01-12 09:19:18 -0800

    Zero task

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,7 +11,7 @@
  */
 char *creat_array(unsigned int size, char c)
 {
-	char *arr;
+	char *str;
 	unsigned int i;
 
 	str = malloc(sizeof(char) * size);
@@ -20,7 +20,7 @@ char *creat_array(unsigned int size, char c)
 
 	for (i = 0; i < size; i++)
 		str[i] = c;
-	return (str)
+	return (str);
 
 
 }
