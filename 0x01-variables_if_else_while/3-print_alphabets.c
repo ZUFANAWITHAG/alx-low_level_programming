e1ea508d4f8a559ed89610ac8e41788182221172
Zufan <zufanawushu@gmail.com>
2024-03-05 13:52:34 -0800

    4rth Task

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - main entry point
+ * Return: Success
+ *
+ */
+
+int main(void)
+{
+	char k = 'a';
+
+	if (k <= 'z')
+	{
+		putchar(k);
+	}
+	else if (k >= 'z')
+	{
+		putchar(k);
+	}
+	putchar('\n');
+	return (0);
+}
