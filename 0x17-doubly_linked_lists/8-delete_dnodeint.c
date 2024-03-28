8e33e550e761474dd260af31c2083a76d5512c0d
Zufan <zufanawushu@gmail.com>
2024-03-28 12:36:13 -0700

    Task eight

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -10,43 +10,40 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-        dlistint_t *h1;
-        dlistint_t *h2;
-        unsigned int k;
+	dlistint_t *h1;
+	dlistint_t *h2;
+	unsigned int k;
 
-        h1 = *head;
+	h1 = *head;
 
-        if (h1 != NULL)
-                while (h1->prev != NULL)
-                        h1 = h1->prev;
+	if (h1 != NULL)
+		while (h1->prev != NULL)
+		h1 = h1->prev;
 
-        k = 0;
+	k = 0;
 
-        while (h1 != NULL)
-        {
-                if (k == index)
-                {
-                        if (k == 0)
-                        {
-                                *head = h1->next;
-                                if (*head != NULL)
-                                        (*head)->prev = NULL;
-                        }
-                        else
-                        {
-                                h2->next = h1->next;
-
-                                if (h1->next != NULL)
+	while (h1 != NULL)
+	{
+		if (k == index)
+		{
+			if (k == 0)
+			{
+				*head = h1->next;
+				if (*head != NULL)
+					(*head)->prev = NULL;
+			}
+			else
+			{
+				h2->next = h1->next;
+				if (h1->next != NULL)
 					h1->next->prev = h2;
-                        }
-
-                        free(h1);
-                        return (1);
-                }
-                h2 = h1;
-                h1 = h1->next;
-                k++;
-        }
-
-        return (-1);
+			}
+			free(h1);
+			return (1);
+		}
+		h2 = h1;
+		h1 = h1->next;
+	k++;
+	}
+	return (-1);
 }
